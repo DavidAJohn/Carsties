c32819147800e722a869f1d819741fbfcdc9e0e9
David John <davidajohn@gmail.com>
2024-02-01 23:20:12 +0000

    Section 3 - completed search service functionality

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDB.Entities;
+using SearchService.Models;
+using System.Text.Json;
+
+namespace SearchService.Data;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDb", 
+                    MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.Queryable<Item>().CountAsync();
+
+        if (count == 0)
+        {
+            Console.WriteLine("Seeding database...");
+
+            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+            if (items is not null)
+            {
+                foreach (var item in items)
+                {
+                    item.CreatedAt = DateTime.UtcNow;
+                    item.UpdatedAt = DateTime.UtcNow;
+                    item.AuctionEnd = DateTime.UtcNow.AddDays(14);
+                }
+
+                await DB.SaveAsync(items);
+            }
+            else
+            {
+                Console.WriteLine("Failed to seed database.");
+            }
+        }
+    }
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,5 @@
+using SearchService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,5 +13,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+	await DbInitializer.InitDb(app);
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
 
 app.Run();
