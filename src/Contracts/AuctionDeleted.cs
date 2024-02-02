a0282be679bd0b25763733963e22ae0d2cb4e076
David John <davidajohn@gmail.com>
2024-02-02 18:31:04 +0000

    Section 4 - added class library for event bus contracts

---
+++ src/Contracts/AuctionDeleted.cs
@@ -0,0 +1,6 @@
+﻿namespace Contracts;
+
+public class AuctionDeleted
+{
+    public required string Id { get; set; }
+}
---
+++ src/Contracts/AuctionUpdated.cs
@@ -0,0 +1,11 @@
+﻿namespace Contracts;
+
+public class AuctionUpdated
+{
+    public required string Id { get; set; }
+    public required string Make { get; set; }
+    public required string Model { get; set; }
+    public required int Year { get; set; }
+    public required string Color { get; set; }
+    public required int Mileage { get; set; }
+}
