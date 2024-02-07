9efbebedd44ed10d04befb11fd904c475aa42d52
David John <davidajohn@gmail.com>
2024-02-07 16:08:22 +0000

    Added further consumers to the Auction and Search services

---
+++ src/Contracts/AuctionFinished.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts;
+
+public class AuctionFinished
+{
+    public bool ItemSold { get; set; }
+    public required string AuctionId { get; set; }
+    public required string Winner { get; set; }
+    public required string Seller { get; set; }
+    public int Amount { get; set; }
+}
