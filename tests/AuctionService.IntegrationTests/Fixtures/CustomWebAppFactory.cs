73cff42c1c2e8c38a62b16776aca321f7e1105fd
David John <davidajohn@gmail.com>
2024-02-13 13:39:47 +0000

    Created a custom web application factory and configured services and testcontainers

---
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
@@ -0,0 +1,44 @@
+﻿using AuctionService.Data;
+using AuctionService.IntegrationTests.Util;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Testcontainers.PostgreSql;
+
+namespace AuctionService.IntegrationTests.Fixtures;
+
+public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
+
+    public async Task InitializeAsync()
+    {
+        await _postgreSqlContainer.StartAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveDbContext<AuctionDbContext>();
+
+            services.AddDbContext<AuctionDbContext>(options =>
+            {
+                options.UseNpgsql(_postgreSqlContainer.GetConnectionString());
+            });
+
+            services.AddMassTransitTestHarness();
+
+            services.EnsureCreated<AuctionDbContext>();
+        });
+    }
+
+    Task IAsyncLifetime.DisposeAsync()
+    {
+        return _postgreSqlContainer.DisposeAsync().AsTask();
+    }
+}
