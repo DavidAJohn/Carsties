7d58f34b01b6b175b214df8b5774ecc99bfe1708
David John <davidajohn@gmail.com>
2024-02-14 17:46:59 +0000

    Section 8 - added a basic navbar component

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,9 +1,9 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-16">
-      <div className="text-3xl font-semibold">
-        Carsties App!
+    <main className="flex flex-col items-center justify-between">
+      <div className="text-xl font-semibold">
+        Main content goes here
       </div>
     </main>
   );
