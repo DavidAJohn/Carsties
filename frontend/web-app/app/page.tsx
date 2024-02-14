32c53d17bbcdbc03b206998a3154f1fc7a946947
David John <davidajohn@gmail.com>
2024-02-14 17:22:18 +0000

    Section 8 - created the Next.js client app

---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-16">
+      <div className="text-3xl font-semibold">
+        Carsties App!
+      </div>
+    </main>
+  );
+}
