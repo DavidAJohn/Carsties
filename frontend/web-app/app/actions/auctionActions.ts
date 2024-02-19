3cdd68a3a2ac7b4ad6eb00a161d52609be7d3cea
David John <davidajohn@gmail.com>
2024-02-19 13:19:33 +0000

    Section 9 - added access token to requests requiring authentication

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { Auction, PagedResult } from "@/types";
+import { getTokenWorkaround } from "./authActions";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
     const response = await fetch(`http://localhost:6001/search${query}`);
@@ -14,13 +15,16 @@ export async function getData(query: string): Promise<PagedResult<Auction>> {
 
 export async function UpdatedAuctionTest() {
     const data = {
-      mileage: Math.floor(Math.random() * 100000) + 1
+        mileage: Math.floor(Math.random() * 100000) + 1
     }
+
+    const token = await getTokenWorkaround();
 
     const res = await fetch('http://localhost:6001/auctions/bbab4d5a-8565-48b1-9450-5ac2a5c4a654', {
         method: 'PUT',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token?.access_token
         },
         body: JSON.stringify(data)
     });
