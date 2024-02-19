230a659b8ec5fe6de905e924a051ca1fc0c9fd02
David John <davidajohn@gmail.com>
2024-02-19 12:39:18 +0000

    Section 9 - created a component to test authentication

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -11,3 +11,21 @@ export async function getData(query: string): Promise<PagedResult<Auction>> {
   
     return response.json();
 }
+
+export async function UpdatedAuctionTest() {
+    const data = {
+      mileage: Math.floor(Math.random() * 100000) + 1
+    }
+
+    const res = await fetch('http://localhost:6001/auctions/bbab4d5a-8565-48b1-9450-5ac2a5c4a654', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (!res.ok) return {status: res.status, message: res.statusText};
+
+    return res.statusText;
+}
