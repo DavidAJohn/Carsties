085840d75543f0eeaf9bb82478f95e6e737e764c
David John <davidajohn@gmail.com>
2024-02-19 11:58:44 +0000

    Section 9 - added login functionality

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { AiOutlineCar } from 'react-icons/ai'
 import Search from './Search'
+import LoginButton from './LoginButton'
+import { getCurrentUser } from '../actions/authActions';
+import UserActions from './UserActions';
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await getCurrentUser();
+
   return (
     <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
         <div className='flex items-center gap-2 text-3xl font-semibold text-red-500'>
@@ -10,7 +15,11 @@ export default function Navbar() {
             <div>Carsties Auctions</div>
         </div>
         <Search />
-        <div>Login</div>
+        {user ? (
+          <UserActions user={user} />
+        ) : (
+          <LoginButton />
+        )}
     </header>
   )
 }
