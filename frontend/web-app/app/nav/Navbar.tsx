9385704440572a968650416d2e3bdc43f636db79
David John <davidajohn@gmail.com>
2024-02-20 12:39:08 +0000

    Turned the navbar site logo into a Home link

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -4,15 +4,18 @@ import Search from './Search'
 import LoginButton from './LoginButton'
 import { getCurrentUser } from '../actions/authActions';
 import UserActions from './UserActions';
+import Link from 'next/link';
 
 export default async function Navbar() {
   const user = await getCurrentUser();
 
   return (
     <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
-        <div className='flex items-center gap-2 text-3xl font-semibold text-red-500'>
-            <AiOutlineCar size={34} />
-            <div>Carsties Auctions</div>
+        <div className='text-3xl font-semibold text-red-500'>
+            <Link href='/' className='flex items-center gap-2 '>
+              <AiOutlineCar size={34} />
+              <div>Carsties Auctions</div>
+            </Link>
         </div>
         <Search />
         {user ? (
