8b2c28d6f68917db7b3f30ce76c0d82b31fb4e34
David John <davidajohn@gmail.com>
2024-02-20 13:07:56 +0000

    Section 10 - added routing to navbar and listings

---
+++ frontend/web-app/app/auctions/update/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Update({params}: {params: {id: string}}) {
+  return (
+    <div>Update for {params.id}</div>
+  )
+}
--- frontend/web-app/hooks/useParamsStore.ts
+++ frontend/web-app/hooks/useParamsStore.ts
@@ -7,6 +7,8 @@ type State = {
     searchTerm: string;
     orderBy: string;
     filterBy: string;
+    seller?: string;
+    winner?: string;
 }
 
 type Actions = {
@@ -20,7 +22,9 @@ const initialState: State = {
     pageCount: 1,
     searchTerm: '',
     orderBy: 'make',
-    filterBy: 'live'
+    filterBy: 'live',
+    seller: undefined,
+    winner: undefined
 }
 
 export const useParamsStore = create<State & Actions>()((set) => ({
