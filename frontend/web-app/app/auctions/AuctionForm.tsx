8d8f7cbd3fc77ab81ee457b1d83f6a16ef7207ef
David John <davidajohn@gmail.com>
2024-02-21 09:47:41 +0000

    Section 10 - added edit auction page

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -5,7 +5,7 @@ import React, { useEffect } from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
 import Input from '../components/Input';
 import DateInput from '../components/DateInput';
-import { createAuction } from '../actions/auctionActions';
+import { createAuction, updateAuction } from '../actions/auctionActions';
 import { usePathname, useRouter } from 'next/navigation';
 import { Auction } from '@/types';
 import toast from 'react-hot-toast';
@@ -17,22 +17,37 @@ type Props = {
 export default function AuctionForm({ auction }: Props) {
     const router = useRouter();
     const pathname = usePathname();
+
     const { control, handleSubmit, setFocus, reset,
         formState: { isSubmitting, isValid } } = useForm({
             mode: 'onTouched'
         });
 
     useEffect(() => {
+        if (auction) {
+            const {make, model, color, mileage, year} = auction;
+            reset({make, model, color, mileage, year});
+        }
         setFocus('make');
-    }, [setFocus])
+    }, [setFocus, reset, auction])
 
     async function onSubmit(data: FieldValues) {
         try {
-            const res = await createAuction(data);
+            let id = '';
+            let res;
+            if (pathname === '/auctions/create/') {
+                res = await createAuction(data);
+                id = res.id;
+            } else {
+                if (auction) {
+                    res = await updateAuction(data, auction.id);
+                    id = auction.id;
+                }
+            }
             if (res.error) {
                 throw res.error;
             }
-            router.push(`/auctions/details/${res.id}`)
+            router.push(`/auctions/details/${id}`)
         } catch (error: any) {
             toast.error(error.status + ' ' + error.message)
         }
--- frontend/web-app/app/auctions/update/[id]/page.tsx
+++ frontend/web-app/app/auctions/update/[id]/page.tsx
@@ -1,7 +1,15 @@
+import Heading from '@/app/components/Heading'
 import React from 'react'
+import AuctionForm from '../../AuctionForm'
+import { getDetailedViewData } from '@/app/actions/auctionActions'
 
-export default function Update({params}: {params: {id: string}}) {
+export default async function Update({params}: {params: {id: string}}) {
+  const data = await getDetailedViewData(params.id);
+
   return (
-    <div>Update for {params.id}</div>
+    <div className='mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg'>
+      <Heading title='Update your auction' subtitle='Please update the details of your car' />
+      <AuctionForm auction={data} />
+    </div>
   )
 }
