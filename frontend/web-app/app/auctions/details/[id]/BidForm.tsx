6347290417b929e9a342b6a3b7812fdf2bb83398
David John <davidajohn@gmail.com>
2024-02-27 16:21:19 +0000

    Section 13 - added a bid form with error handling and conditional display

---
+++ frontend/web-app/app/auctions/details/[id]/BidForm.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+type Props = {
+    auctionId: string;
+    highBid: number;
+}
+
+import { placeBidForAuction } from '@/app/actions/auctionActions';
+import { useBidStore } from '@/hooks/useBidStore';
+import { currencyFormatter } from '@/lib/currencyFormatter';
+import React from 'react'
+import { FieldValues, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+
+export default function BidForm({ auctionId, highBid }: Props) {
+    const {register, handleSubmit, reset, formState: {errors}} = useForm();
+    const addBid = useBidStore(state => state.addBid);
+
+    function onSubmit(data: FieldValues) {  
+        placeBidForAuction(auctionId, +data.amount).then(bid => {
+            if (bid.error) throw bid.error;
+
+            addBid(bid);
+            reset();
+        }).catch(err => toast.error(err.message));
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className='flex items-center border-2 rounded-lg py-2'>
+            <input 
+                type="number" 
+                {...register('amount')}
+                className='input-custom text-sm text-gray-600'
+                placeholder={`Enter your bid (minimum bid is ${currencyFormatter.format(highBid + 1)})`}
+            />
+        </form>
+    )
+}
--- frontend/web-app/app/auctions/details/[id]/BidList.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidList.tsx
@@ -8,6 +8,9 @@ import { User } from 'next-auth'
 import React, { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import BidItem from './BidItem'
+import { currencyFormatter } from '@/lib/currencyFormatter'
+import EmptyFilter from '@/app/components/EmptyFilter'
+import BidForm from './BidForm'
 
 type Props = {
     user: User | null
@@ -19,6 +22,9 @@ export default function BidList({ user, auction }: Props) {
 
     const bids = useBidStore(state => state.bids)
     const setBids = useBidStore(state => state.setBids)
+    const highBid = bids.reduce((prev, current) => (prev > current.amount) 
+        ? prev 
+        : current.amount, 0)
 
     useEffect(() => {
         getBidsForAuction(auction.id)
@@ -26,7 +32,6 @@ export default function BidList({ user, auction }: Props) {
                 if (res.error) {
                     throw res.error
                 }
-
                 setBids(res as Bid[])
             }).catch(err => {
                 toast.error(err.message)
@@ -38,11 +43,36 @@ export default function BidList({ user, auction }: Props) {
     if (loading) return <div>Loading...</div>
 
     return (
-        <div className='border-2 rounded-lg p-2 bg-gray-100'>
-          <Heading title='Bids' />
-          {bids.map(bid => (
-            <BidItem key={bid.id} bid={bid} />
-          ))}
+        <div className='rounded-lg shadow-md'>
+            <div className='py-2 px-4 bg-white'>
+                <div className='sticky top-0 bg-white p-2'>
+                    <Heading title={`Current high bid is: ${currencyFormatter.format(highBid)}`} />
+                </div>
+            </div>
+            <div className='overflow-auto h-[400px] flex flex-col-reverse px-2'>
+                {bids.length === 0 ? (
+                    <EmptyFilter title='No bids yet' subtitle='Please make a bid' />
+                ) : (
+                    <>
+                        {bids.map(bid => (
+                            <BidItem key={bid.id} bid={bid} />
+                        ))}
+                    </>
+                )}
+            </div>
+            <div className='px-2 pb-2 text-gray-500'>
+                {!user ? (
+                    <div className='flex items-center justify-center p-2 text-lg font-semibold'>
+                        Please login to make a bid
+                    </div>
+                ) : user && user.username === auction.seller ? (
+                    <div className='flex items-center justify-center p-2 text-lg font-semibold'>
+                        You cannot bid on your own auction
+                    </div>
+                ) : (
+                    <BidForm auctionId={auction.id} highBid={highBid} />
+                )}
+            </div>
         </div>
     )
 }
