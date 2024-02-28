7e2247aca6250682eac19c9ccf8e3d9fc7c5912f
David John <davidajohn@gmail.com>
2024-02-28 12:24:36 +0000

    Section 13 - added toasts for creation and completion of auctions

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -3,14 +3,20 @@
 import {ReactNode, useEffect, useState} from 'react';
 import {HubConnection, HubConnectionBuilder} from "@microsoft/signalr";
 import {useAuctionStore} from "@/hooks/useAuctionStore";
-import {Bid} from "@/types";
+import {Auction, AuctionFinished, Bid} from "@/types";
 import { useBidStore } from '@/hooks/useBidStore';
+import { User } from 'next-auth';
+import toast from 'react-hot-toast';
+import AuctionCreatedToast from '../components/AuctionCreatedToast';
+import { getDetailedViewData } from '../actions/auctionActions';
+import AuctionFinishedToast from '../components/AuctionFinishedToast';
  
 type Props = {
-    children: ReactNode;
+    children: ReactNode,
+    user: User | null
 }
  
-export default function SignalRProvider({children}: Props) {
+export default function SignalRProvider({children, user}: Props) {
     const [connection, setConnection] = useState<HubConnection | null>(null);
     const setCurrentPrice = useAuctionStore(state => state.setCurrentPrice);
     const addBid = useBidStore(state => state.addBid);
@@ -36,14 +42,32 @@ export default function SignalRProvider({children}: Props) {
                         }
                         addBid(bid);
                     })
-                })
-                .catch(error=>console.log(error))
+
+                    connection.on('AuctionCreated', (auction: Auction) => {
+                        if (user?.username !== auction.seller) {
+                            return toast(<AuctionCreatedToast auction={auction} />, {duration: 5000})
+                        }
+                    })
+
+                    connection.on('AuctionFinished', (finishedAuction: AuctionFinished) => {
+                        const auction = getDetailedViewData(finishedAuction.auctionId);
+                        return toast.promise(auction, {
+                            loading: 'Loading',
+                            success: (auction) => 
+                                <AuctionFinishedToast 
+                                    finishedAuction={finishedAuction} 
+                                    auction={auction}
+                                />,
+                            error: (err) => 'Auction finished!'
+                        }, {success: {duration: 10000, icon: null}})
+                    })
+                }).catch(error => console.log(error));
         }
         
         return () => {
             connection?.stop();
         }
-    }, [connection, setCurrentPrice, addBid])
+    }, [connection, setCurrentPrice, addBid, user?.username])
     
     return (
         children
