4d4dd6b32d3d5fd272dc9e7ecbfca29116a872f8
David John <davidajohn@gmail.com>
2024-03-01 14:36:58 +0000

    Section 14 - moved react date picker stylesheet import to globals.css

--- frontend/web-app/app/components/DateInput.tsx
+++ frontend/web-app/app/components/DateInput.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { UseControllerProps, useController } from 'react-hook-form'
-import 'react-datepicker/dist/react-datepicker.css';
 import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
 
 type Props = {
